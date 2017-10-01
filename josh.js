e2714b7bd04708f8ec3b5dd707d2eea0673b2151
Josh Admin <thegiantalfredo3@gmail.com>
2017-10-01 10:05:35 -0700

    some mild progress

--- josh.js
+++ josh.js
@@ -1,13 +1,10 @@
 var headerNav = document.getElementsByClassName('navigation');
 
 for(var i = 0; i < headerNav.length; i++) {
-  if (headerNav[i].getAttribute('active') == true) {
-    headerNav[i].className += (' active');
-  }
-
   headerNav[i].addEventListener(
     "click",
     function() {
+      
     },
     false
   );
